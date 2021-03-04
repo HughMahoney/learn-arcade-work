41d79974ad653db87655b830d268943fc40a7eb1
HughMahoney <47137792+HughMahoney@users.noreply.github.com>
2021-03-04 12:39:48 +1030

    done
    done

--- Lab 07 - User Control/lab_07.py
+++ Lab 07 - User Control/lab_07.py
@@ -1,3 +1,8 @@
+############################################################################
+# MOVE WITH KEYBOARD, CLICK WITH MOUSE TO MAKE BLACKHOLE AND TRIGGER SOUND #
+############################################################################
+
+
 
 import arcade
 
@@ -17,17 +22,36 @@ class BlackHole:
         arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, arcade.color.BLACK)
 
 class TennisBall:
-    def __init__(self, position_x, position_y, radius, color):
+    def __init__(self, position_x, position_y, change_x, change_y, radius, color):
 
         # Take the parameters of the init function above, and create instance variables out of them.
         self.position_x = position_x
         self.position_y = position_y
+        self.change_x = change_x
+        self.change_y = change_y
         self.radius = radius
         self.color = color
 
     def draw(self):
         """ Draw the balls with the instance variables we have. """
         arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, self.color)
+
+    def update(self):
+        # Move the ball
+        
+        self.position_y += self.change_y
+        self.position_x += self.change_x
+        if self.position_x < self.radius:
+            self.position_x = self.radius
+
+        if self.position_x > SCREEN_WIDTH - self.radius:
+            self.position_x = SCREEN_WIDTH - self.radius
+
+        if self.position_y < self.radius:
+            self.position_y = self.radius
+
+        if self.position_y > SCREEN_HEIGHT - self.radius:
+            self.position_y = SCREEN_HEIGHT - self.radius
 
 
 class MyGame(arcade.Window):
@@ -44,10 +68,13 @@ class MyGame(arcade.Window):
         #arcade.set_background_color(arcade.color.ASH_GREY)
 
         # Create our ball
-        self.ball = TennisBall(50, 50, 23, arcade.color.GREEN)
+        self.ball = TennisBall(50, 50, 0, 0, 23, arcade.color.GREEN)
         self.hole = None
         self.background = None
         self.background = arcade.load_texture("marioBackground.jpg")
+
+        
+            
 
 
 
@@ -60,13 +87,13 @@ class MyGame(arcade.Window):
         if self.hole:
             self.hole.draw()
 
+    def update(self, delta_time):
+            self.ball.update()
+
         
 
 
-    def on_mouse_motion(self, x, y, dx, dy):
-        """ Called to update our objects. Happens approximately 60 times per second."""
-        self.ball.position_x = x
-        self.ball.position_y = y
+    
 
     def on_mouse_press(self, x, y, button, modifiers):
     
@@ -80,7 +107,9 @@ class MyGame(arcade.Window):
     def on_key_press(self, key, modifiers):
         """ Called whenever the user presses a key. """
         if key == arcade.key.LEFT:
+            
             self.ball.change_x = -MOVEMENT_SPEED
+            print(self.ball.change_x, self.ball.position_x)
         elif key == arcade.key.RIGHT:
             self.ball.change_x = MOVEMENT_SPEED
         elif key == arcade.key.UP:
